f81ce9b33b050c9fee9003e550029338a47ff1f1
jisoolee <swang.jislee@gmail.com>
2022-07-16 13:41:22 -0400

    Update _landing.js

--- _sass/_landing.js
+++ _sass/_landing.js
@@ -1,4 +1,4 @@
-gsap.set('.main', {position:'fixed', background:'#fff', width:'100%', maxWidth:'1200px', height:'100%', top:0, left:'50%', x:'-50%'})
+/*gsap.set('.main', {position:'fixed', background:'#fff', width:'100%', maxWidth:'1200px', height:'100%', top:0, left:'50%', x:'-50%'})
 gsap.set('.scrollDist', {width:'100%', height:'200%'})
 gsap.timeline({scrollTrigger:{trigger:'.scrollDist', start:'top top', end:'bottom bottom', scrub:1}})
     .fromTo('.sky', {y:0},{y:-200}, 0)
@@ -13,3 +13,4 @@ $('#arrowBtn').on('mouseenter', (e)=>{ gsap.to('.arrow', {y:10, duration:0.8, ea
 $('#arrowBtn').on('mouseleave', (e)=>{ gsap.to('.arrow', {y:0, duration:0.5, ease:'power3.out', overwrite:'auto'}); })
 $('#arrowBtn').on('click', (e)=>{ gsap.to(window, {scrollTo:innerHeight, duration:1.5, ease:'power1.inOut'}); }) 
 // scrollTo requires the ScrollTo plugin (not to be confused w/ ScrollTrigger)
+*/
